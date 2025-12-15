77105eba56335b46361a8e8de62fcf49a64b9ef6
Alex W <alexwh@users.noreply.github.com>
2025-12-15 00:58:40 +0000

    init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/alexwh/vaxee-read-battery
+
+go 1.25.5
+
+require (
+	github.com/sstallion/go-hid v0.15.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+import "log"
+import "fmt"
+import "os"
+import "github.com/sstallion/go-hid"
+
+func main() {
+	log.SetPrefix("vaxee-read-battery: ")
+	path := ""
+
+	if err := hid.Init(); err != nil {
+		log.Fatal(err)
+	}
+
+	hid.Enumerate(0x3057, hid.ProductIDAny, func(info *hid.DeviceInfo) error {
+		if info.Usage == 0x01  && info.UsagePage == 0xff05 {
+			fmt.Fprintf(os.Stderr, "ID %04x:%04x %s %s (0x%x 0x%x): %s\n",
+				info.VendorID,
+				info.ProductID,
+				info.MfrStr,
+				info.ProductStr,
+				info.Usage,
+				info.UsagePage,
+				info.Path,
+				)
+			path = info.Path
+		}
+		return nil
+	})
+
+	if path == "" {
+		log.Fatal("no mouse device found")
+	}
+
+	d, err := hid.OpenPath(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	b := make([]byte, 64)
+	b[0] = 0x0e  // report id
+	b[1] = 0xa5  // header
+	b[2] = 0x0b  // command id
+	b[3] = 0x01  // read (0x01) or write (0x02)
+	b[4] = 0x01  // ret len
+	if _, err := d.SendFeatureReport(b); err != nil {
+		log.Println("SendFeatureReport: ", err)
+	}
+
+	if _, err := d.GetFeatureReport(b); err != nil {
+		log.Fatal("GetFeatureReport: ", err)
+	}
+
+	// number is in 5% increments
+	fmt.Printf("%d\n", b[5]*5)
+
+	if err := hid.Exit(); err != nil {
+		log.Fatal(err)
+	}
+}
